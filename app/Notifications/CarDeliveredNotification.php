2e47e58f266850393101ed8b5ccaa0ba4a022e0e
thezaidbintariq <57444558+thezaidbintariq@users.noreply.github.com>
2025-10-21 15:09:22 +0500

    Message reformatted

--- app/Notifications/CarDeliveredNotification.php
+++ app/Notifications/CarDeliveredNotification.php
@@ -27,13 +27,11 @@ class CarDeliveredNotification extends Notification implements ShouldQueue
     {
         return <<<MSG
 تم تسليم سيارتك
-شكرا لزيارتكم مركز مبيعات الدرعية،
-+966 56 520 6244للاقتراحات والشكاوى على خدمة صف السيارات يرجى الاتصال على الرقم 
+شكرا لزيارتكم مركز مبيعات الدرعية
 رافقتكم السلامة
 
 Your car has been delivered.
 Thank you for visiting Diriyah Sales Center.
-For suggestions and complaints about the valet services please contact +966 59 940 9500
 We wish you a safe journey.
 MSG;
     }
